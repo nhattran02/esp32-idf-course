82acc00385cfbaddfdff5e6b2c70498d26b9e0e1
nhatVNU <tranminhnhat20022002@gmail.com>
2023-02-12 21:32:37 +0700

    add random_gen_dice

---
+++ 4_random_gen_dice/main/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+//for idfversion 4.x use #include "esp_system.h"
+#include "esp_random.h"
+#include "math.h"
+
+#define TAG "DICE"
+
+int dice_role(){
+    int random=esp_random();
+    int positiveNumber = abs(random);
+    int diceNumber = (positiveNumber%6)+1;
+    return diceNumber;
+}
+void app_main(void){
+    while (1)
+    {
+        vTaskDelay(200/portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "random number %d", dice_role());
+    }
+    
+  
+}
