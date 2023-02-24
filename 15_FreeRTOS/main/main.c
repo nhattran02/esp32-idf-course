449f0039055e793752d5a059cf978285969ae8d9
nhatVNU <tranminhnhat20022002@gmail.com>
2023-02-24 14:22:21 +0700

    Update UART

--- 15_FreeRTOS/main/main.c
+++ 15_FreeRTOS/main/main.c
@@ -7,14 +7,39 @@
 #define LED1 2
 #define LED2 4 
 
-void task1(void *params){
+typedef struct {
+    int led;
+    int time;
+}LEDParams;
+
+void setup(){
+    gpio_set_direction(LED1, GPIO_MODE_OUTPUT);
+    gpio_set_direction(LED2, GPIO_MODE_OUTPUT);
+}
+void BlinkTask(void *params){
+    LEDParams *l = (LEDParams*)params;
     while(1){
-        printf("LED1 ON\n");
-        vTaskDelay(200/portTICK_PERIOD_MS);
-        printf("LED1 OFF\n");
-        vTaskDelay(200/portTICK_PERIOD_MS);
+        gpio_set_level(l->led, 1);
+        vTaskDelay(l->time/portTICK_PERIOD_MS);
+        gpio_set_level(l->led, 0);
+        vTaskDelay(l->time/portTICK_PERIOD_MS);
     }
 }
 void app_main(void){
-    xTaskCreate(&task1, "Task 1", 1024*2, NULL, 2, NULL);
+    LEDParams ledParams1={
+        .led = LED1,
+        .time = 200
+    };
+    LEDParams ledParams2={
+        .led = LED2,
+        .time = 200
+    };
+    setup();
+    xTaskCreate(&BlinkTask, "Blink LED 1", 1024*2, &ledParams1, 2, NULL);
+    xTaskCreate(&BlinkTask, "Blink LED 2", 1024*2, &ledParams2, 2, NULL);
+    //it never go into while loop :) 
+    while (true)
+    {
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
 }
